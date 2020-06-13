00e509b5184649d87fc2dd2afa052cfcb23f76c2
husnutapan <husnu.tapan@gmail.com>
2020-06-14 00:31:35 +0300

    Updates

---
+++ precache-manifest.ce22d746d0a097074834164a4bea5b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5974ea5fd8314640205102d96743ec",
+    "url": "/player-cards/index.html"
+  },
+  {
+    "revision": "c1e649d412220e7d6783",
+    "url": "/player-cards/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "726aa08c7c44638177ec",
+    "url": "/player-cards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c1e649d412220e7d6783",
+    "url": "/player-cards/static/js/2.8178d380.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/player-cards/static/js/2.8178d380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726aa08c7c44638177ec",
+    "url": "/player-cards/static/js/main.83c2c9b3.chunk.js"
+  },
+  {
+    "revision": "c401f240f7befae1cb25",
+    "url": "/player-cards/static/js/runtime-main.8e12ca95.js"
+  }
+]);
